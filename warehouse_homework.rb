427e43e261e8c53efdb805891abfb61cfad6cb05
User <user@Users-MacBook-Pro-5.local>
2015-11-23 09:19:13 +0000

    Final Commit

--- warehouse_homework.rb
+++ warehouse_homework.rb
@@ -27,6 +27,107 @@ WAREHOUSE = [
   item:"cookie jar",
   distance:30 },
 
+  {location:"b9",
+   item:"deodorant",
+   distance:29 },
+
+   {location:"c2",
+    item:"drill press",
+    distance:12 },
+
+    {location:"c6",
+     item:"face wash",
+     distance:16 },
+
+     {location:"a9",
+      item:"glow stick",
+      distance:2 },
+
+   {location:"a4",
+    item:"hanger",
+    distance:7 },   
+
+   {location:"c10",
+    item:"leg warmers",
+    distance:20 }, 
+
+    {location:"a8",
+     item:"model car",
+     distance:3 },
+
+   {location:"b5",
+    item:"nail filer",
+    distance:25 },  
+
+   {location:"a1",
+    item:"needle",
+    distance:10 }, 
+
+    {location:"c7",
+     item:"paint brush",
+     distance:17 },
+
+    {location:"b4",
+     item:"photo album",
+     distance:24 }, 
+
+     {location:"b3",
+      item:"picture frame",
+      distance:23 },
+
+      {location:"a10",
+       item:"rubber band",
+       distance:1 },
+
+       {location:"a5",
+        item:"rubber duck",
+        distance:6 },
+
+       {location:"c1",
+        item:"rusty nail",
+        distance:11 }, 
+
+       {location:"b2",
+        item:"sharpie",
+        distance:22 }, 
+
+      {location:"c9",
+       item:"shoe lace",
+       distance:19 },
+
+       {location:"a6",
+        item:"shovel",
+        distance:5 },
+
+       {location:"a2",
+        item:"stop sign",
+        distance:9 }, 
+
+        {location:"c5",
+         item:"thermometer",
+         distance:15 },
+
+          {location:"b1",
+          item:"thermometer",
+          distance:21 },
+
+         {location:"b1",
+          item:"tyre swing",
+          distance:21 }, 
+
+      {location:"b8",
+       item:"tissue box",
+       distance:28 },    
+
+       {location:"b6",
+        item:"tooth paste",
+        distance:26 },
+
+        {location:"c4",
+         item:"word search",
+         distance:14 },
+
+
 ]
 # Problem 1________________________________
 def item_at_bay(bay_number)
@@ -86,26 +187,38 @@ end
 # Problem 6_________________________________________
 
 
-def order_from_entrance(*items)
+# def order_from_entrance(*items)
 
-  required_bays = WAREHOUSE.select do |bay| 
+#   required_bays = WAREHOUSE.select do |bay| 
       
-  items.include?(bay[:item])
+#   items.include?(bay[:item])
 
-  end
+#   end
+
+
+# ordered_route = required_bays.sort_by { |bay| bay[:distance] }
+
+# ordered_route.map{|bay| "#{bay[:location]} #{bay[:item]}" }.join(" - ")
+
+# end  
+
+
+# Alternate Problem 6
+
+def order_from_entrance(array)
+
+required_bays = WAREHOUSE.select do |bay| 
+      
+array.include?(bay[:item])
+
+ end
 
 
 ordered_route = required_bays.sort_by { |bay| bay[:distance] }
 
 ordered_route.map{|bay| "#{bay[:location]} #{bay[:item]}" }.join(" - ")
-  # order_array = required_bays.map{|bay| bay[:distance, :location]}
+
 end  
-
-
-
-
-
-
 
 
 
--- warehouse_homework_runner.rb
+++ warehouse_homework_runner.rb
@@ -11,13 +11,23 @@ require_relative  './warehouse_homework'
 # found_bays = multiple_bays("bath fizzers", "bookmark", "cookie jar")
 # puts "required bays are #{found_bays}"
 
-found_items = multiple_items("b7", "a3")
-puts "required items are #{found_items}"
+# found_items = multiple_items("b7", "a3")
+# puts "required items are #{found_items}"
 
 # distance = distance_between_items("b7", "a3")
-# puts "the furthest distance between items is #{distance}"
+# puts "the furthest distance between items is #{distance}"]
+item_array = []
+while true
+puts "Please enter items required, when finished enter stop"
+answer = gets.chomp.downcase
+item_array = item_array.push(answer)
+if answer == "stop"
+break
+end
+end
 
-route = order_from_entrance("bath fizzers", "blouse", "bookmark")
+
+route = order_from_entrance(item_array)
 puts route
 
 
